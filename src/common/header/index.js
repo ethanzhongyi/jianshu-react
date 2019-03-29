7c30ddfa28b604c78f605c7d2356dcc788793067
zhongyi <zhongyi@giec.com>
2019-03-29 12:25:59 +0800

    优化请求次数

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -57,7 +57,7 @@ class Header extends Component {
   }
 
   render() {
-    const {focused, handleInputFocus, handleInputBlur} = this.props
+    const {focused, handleInputFocus, handleInputBlur, list} = this.props
     return (
       <HeaderWrapper>
         <Logo />
@@ -76,7 +76,7 @@ class Header extends Component {
             >
               <NavSearch 
                 className={focused ? 'focused' : ''}
-                onFocus={handleInputFocus}
+                onFocus={() => handleInputFocus(list)}
                 onBlur={handleInputBlur}
                 ></NavSearch>
             </CSSTransition>
@@ -108,8 +108,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleInputFocus() {
-      dispatch(actionCreators.getList());
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {

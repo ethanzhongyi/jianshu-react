5a0cef328150c0028347270373c24a8b7de1b8a5
zhongyi <zhongyi@giec.com>
2019-03-29 20:52:35 +0800

    专题toplist组件

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { TopicWrapper, TopicItem } from '../style';
 
 class Topic extends Component {
     render() {
+      const { list } = this.props
       return (
-        <div>Topic==</div>
+        <TopicWrapper>
+          {
+            list.map((item) => {
+              return (
+                <TopicItem key={item.get('id')}>
+                  <img 
+                    className='topic-pic' alt=''
+                    src={item.get('imgUrl')}
+                  />
+                  {item.get('title')}
+                </TopicItem>
+              )
+            }) 
+          }  
+        </TopicWrapper>
       )
     }
 }
+const mapState =(state) => {
+  return {
+    list: state.getIn(['home','topicList'])
+  }
+};
 
-export default Topic;
+export default connect(mapState, null)(Topic);

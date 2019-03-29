c3dfe056edf3007b85c1bf92fd14ed6c27c1fc48
zhongyi <zhongyi@giec.com>
2019-03-29 13:31:57 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './common/header';
 import { Globalstyle } from './statics/iconfont/iconfont'
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -11,6 +12,12 @@ class App extends Component {
         <Globalstyle></Globalstyle>
         <Provider store={store}>
           <Header />
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact render={() => <div>home</div>}></Route>
+              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
         </Provider>
       </Fragment>    
     )

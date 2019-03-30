43c75d32e32dc757e210a73efa38ca7d160680d6
zhongyi <zhongyi@giec.com>
2019-03-30 10:52:39 +0800

    add articleList

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListItem, ListInfo } from '../style';
 
 class List extends Component {
     render() {
+      const { list } = this.props;
       return (
-        <div>List==</div>
+        <div>
+          {
+            list.map((item) => {
+              return (
+                <ListItem key={item.get('id')}>
+                  <img className='pic' alt='' src={item.get('imgUrl')}/>
+                  <ListInfo>
+                    <h3 className='title'>{item.get('title')}</h3>
+                    <p className='desc'>{item.get('desc')}</p>
+                  </ListInfo>
+                </ListItem>
+              )
+            })
+          }
+        </div>
       )
+      
     }
 }
 
-export default List;
+const mapState= (state) => {
+  return {
+    list: state.getIn(['home', 'articleList']) 
+  }
+};
+
+export default connect(mapState, null)(List);
